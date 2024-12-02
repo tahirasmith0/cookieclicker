ed44b01cef1934d63b712fe150cdf07a245d4565
tahirasmith0 <tahirasmith0@gmail.com>
2024-12-02 14:50:13 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,15 @@ const urlsToCache = [
     '/style.css'   // Add other files like CSS or images here if needed
 ];
 
+if ('serviceWorker' in navigator) {
+   navigator.serviceWorker.register('/service-worker.js')
+   .then(registration => {
+      console.log('Service Worker registered:', registration);
+   })
+   .catch(error => {
+      console.error('Service Worker registration failed:', error);
+   });
+}
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
