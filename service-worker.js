7f815fc9262a9b6450904b517e9923eb8c2d68c4
tahirasmith0 <tahirasmith0@gmail.com>
2024-12-02 11:41:59 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'cookie-clicker-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/game.js'
+    // Add other files like CSS or images here if needed
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return the cached resource if found, or fetch from the network
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
